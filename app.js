4ae9648e625f4178d25f44102509c4fde1098c38
gorsar75 <92424358+gorsar75@users.noreply.github.com>
2022-01-07 18:55:56 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+let users = [
+    {
+        Name: "Addison Roberts",
+        Address: "210 Fidel Square West Sabina, PA 74688 - 1088",
+        Email: "pcummerata@tillman.com",
+        Username: "addisorts",
+        Password: "21df1c0e"
+    },
+
+    {
+        Name: "Veronica Johnson",
+        Address: "37883 Bernier Harbors Abelburgh, KS 71420-7448",
+        Email: "cronin.pauline@shields.info",
+        Username: "veronison",
+        Password: "9016b650"
+    },
+
+    {
+        Name: "Gabriela Diaz",
+        Address: "679 Towne Flats Apt. 376 Brendanchester, TN 23251-8361",
+        Email: "rbatz@yahoo.com",
+        Username: "gabriiaz",
+        Password: "39623367"
+    },
+
+    {
+        Name: "Adelynn Cox",
+        Address: "780 Delbert Walks West Vito, NC 53825-2013",
+        Email: "herminia.hauck@gmail.com",
+        Username: "adelynox",
+        Password: "3ddb73ad"
+    },
+
+    {
+        Name: "Camilla Morales",
+        Address: "35505 Hills Mountain Apt. 742South Selina, OH 52624",
+        Email: "herminia.hauck@gmail.com",
+        Username: "camiles",
+        Password: "273e48e8"
+    }
+]
+for (const arrkey in users) {
+    console.log(arrkey, users[arrkey]);
+
+}
